b6b8585a37b24e6f4587e36458e01ab0b125ff3b
Souls Syntax Nix <souls.syntax@gmail.com>
2025-11-12 12:49:33 +0530

    Lecture 20

--- powerRangers150/Lecture_20/part1/app.js
+++ powerRangers150/Lecture_20/part1/app.js
@@ -1,1 +1,65 @@
-sepyt = new Promise
+function step1() {
+  return new Promise(function(resolve , reject) {
+    setTimeout(function() {
+      resolve("Sekected Image")
+      //reject("Yep")
+    }, 4000)
+  })
+} 
+
+
+//let p1 = step1()
+//p1
+//.then(function(data) {console.log(data)})
+//.catch(function(err) {console.log(data)})
+//.finally(function() {console.log("What???")})
+
+function step2() {
+  console.log("please wait applying filter toe the ${image}")
+  return new Promise(function(resolve , reject) {
+    setTimeout(function() {
+      resolve("Filtered Image")
+      //reject("Yep")
+    }, 4000)
+  })
+
+}
+function step3() {
+  console.log("Uploading ${caption}")
+  return new Promise(function(resolve , reject) {
+    setTimeout(function() {
+      resolve("Filtered Image")
+      //reject("Yep")
+    }, 4000)
+  })
+
+}
+function step4() {
+  console.log("uploading")
+  return new Promise(function(resolve , reject) {
+    setTimeout(function() {
+      resolve("Uploading")
+      //reject("Yep")
+    }, 4000)
+  })
+
+}
+
+
+
+step1()
+.then(function(data) {
+  console.log(data)
+  return step2(data)
+})
+.then(function(data){
+  console.log(data)
+  return step3(data)
+})
+.then(function(data){
+  console.log(data)
+  return step4(data)
+})
+.then(function(data){
+  console.log(data)
+})
